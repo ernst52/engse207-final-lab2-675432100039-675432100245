c8d5e32075a3cfd6123a3e6ba407663d4907ef17
siri-se <rattanarin.siri@gmail.com>
2026-03-16 20:52:27 +0700

    config.js

---
+++ frontend/config.js
@@ -0,0 +1,5 @@
+window.APP_CONFIG = {
+  AUTH_URL: 'https://auth-service-production-2f4d.up.railway.app',
+  TASK_URL: 'https://task-service-production-793b.up.railway.app',
+  USER_URL: 'https://user-task-production.up.railway.app'
+};
